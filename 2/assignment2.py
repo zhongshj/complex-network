abe131ddda76647f33f8874a11909909f4d38115
abk <abk@wlan-145-94-152-105.wlan.tudelft.nl>
2017-04-04 21:31:56 +0200

    convert time

--- 2/assignment2.py
+++ 2/assignment2.py
@@ -7,10 +7,21 @@ Created on Tue Mar 28 09:56:27 2017
 """
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-import pandas
+import math
+from datetime import datetime
+import pytz
 
+def degree(mat,i):
+    return sum(mat[i])
 
+def gen_mat(node1,node2):
+    mat = np.zeros([size,size])
+    for i in list(range(np.size(node1))):
+        mat[node1[i]-1,node2[i]-1] = 1
+        mat[node2[i]-1,node1[i]-1] = 1
+    return mat
 #%% read data
 
 data = []
@@ -24,4 +35,24 @@ data = np.array(data)
 sender = data.T[0]
 receiver = data.T[1]
 timestamp = data.T[2]
-#%%
+
+#%% convert timestamp to month, day, weekday, hour, minute
+
+time_formulate = np.zeros([np.size(timestamp),5])
+for i in range(np.size(timestamp)):
+    time_formulate[i,0] = datetime.fromtimestamp(timestamp[i]-3600*7,pytz.timezone('UTC')).month
+    time_formulate[i,1] = datetime.fromtimestamp(timestamp[i]-3600*7,pytz.timezone('UTC')).day
+    time_formulate[i,2] = datetime.fromtimestamp(timestamp[i]-3600*7,pytz.timezone('UTC')).weekday()+1
+    time_formulate[i,3] = datetime.fromtimestamp(timestamp[i]-3600*7,pytz.timezone('UTC')).hour
+    time_formulate[i,4] = datetime.fromtimestamp(timestamp[i]-3600*7,pytz.timezone('UTC')).minute    
+#%% create matrix
+size = np.max([np.max(sender),np.max(receiver)])
+print("number of nodes: ",size) #number of nodes
+
+#create link matrix
+m = np.zeros([size,size])
+m_t = np.zeros([size,size])
+for i in range(np.size(sender)):
+    m[sender[i]-1,receiver[i]-1] = m[sender[i]-1,receiver[i]-1] + 1
+
+    
--- readme.md
+++ readme.md
@@ -1,11 +1,13 @@
 http://snap.stanford.edu/data/CollegeMsg.html
 
+## Topology
+
 ### General
 
 1. general mailing frequency by time
 2. user activity by time(short-term & long-term)
 3. reciprocate(effective communication(quality))
-4. user 
+4. ​
 
 
 
@@ -22,3 +24,14 @@ http://snap.stanford.edu/data/CollegeMsg.html
 
 1. special activity on weekends, holidays
 2. make static network by period(weeks, months), or weighted edges to see network characteristics
+
+
+
+Go deeper（看论文）（creative）
+
+## Data driven
+
+Model: SS （如果不连续的话会有问题）或者SR，SI
+
+1. immunization
+2. link weight -> 计算infection rate
